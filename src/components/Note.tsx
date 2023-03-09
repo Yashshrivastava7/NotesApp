54f403ab4678928a1cb42cbe9d2df155c884bb25
Yash Shrivastava <yash.shrivastava77@gmail.com>
2023-03-09 22:08:07 +0530

    Added delete button for each note

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,11 +1,18 @@
 import "../styles/Note.css";
 import { NoteObject } from "../types/Types";
 
+const handleClick = () => {
+
+}
+
 function Note(props: NoteObject) {
   return (
     <div className="each-note">
       <h2>{props.title}</h2>
       <p>{props.body}</p>
+      <button className="note-button" onClick={handleClick}>
+        Delete note
+      </button>
     </div>
   );
 }
