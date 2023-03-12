4fa1ddba1f2f7cb01bb1ebd2e4deaf68a3390a93
Yash Shrivastava <yash.shrivastava77@gmail.com>
2023-03-12 19:47:40 +0530

    Prettified code and removed unused stuff

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -17,9 +17,7 @@ function App() {
   //     </div>
   //   </div>
   // );
-  return (
-    <Login/>
-  );
+  return <Login />;
 }
 
 export default App;
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,13 +1,10 @@
-import { useEffect, useState } from "react" ;
-import "../styles/Login.css" ;
-
-const STUB_USERNAME: string = "username";
-const STUB_PASSWORD: string = "password";
+import { useEffect, useState } from "react";
+import "../styles/Login.css";
 
 type UserPassType = {
-  username: string,
-  password: string
-}
+  username: string;
+  password: string;
+};
 
 function Login() {
   const [data, setData] = useState<UserPassType[]>([]);
@@ -22,13 +19,13 @@ function Login() {
       setIsClicked(false);
     }
   }, [isClicked]);
-  
-  const handleUser = (e : React.ChangeEvent<HTMLInputElement>) => {
-    setId(_ => e.target.value);
-  }
-  const handlePass = (e : React.ChangeEvent<HTMLInputElement>) => {
-    setPass(_ => e.target.value);
-  }
+
+  const handleUser = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setId((_) => e.target.value);
+  };
+  const handlePass = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPass((_) => e.target.value);
+  };
   const handleLogin = () => {
     const filtered = data.filter((e) => e.username === id);
     if (filtered.length === 0) {
@@ -39,28 +36,28 @@ function Login() {
     if (filtered[0].password === pass) {
       setLoginMessage("Login Successful");
     } else {
-      setLoginMessage("Incorrect pass")
+      setLoginMessage("Incorrect pass");
     }
-  }
+  };
   const handleSignUp = () => {
     console.log("User signed up!");
-    console.log("Username: "+id+" Password: "+pass);
+    console.log("Username: " + id + " Password: " + pass);
     const filtered = data.filter((e) => e.username === id);
     if (filtered.length !== 0) {
       setLoginMessage("User already exists!");
       setIsClicked(true);
       return;
     }
-    setData((old: any): any => {
-      return [...old, {username: id, password: pass}]
+    setData((old) => {
+      return [...old, { username: id, password: pass }];
     });
     setIsClicked(true);
-  }
+  };
   return (
     <div className="login-holder">
       <h1>Login or Sign Up</h1>
-      <input type="text" placeholder="Username" onChange={handleUser}/>
-      <input type="password" placeholder="Password" onChange={handlePass}/>
+      <input type="text" placeholder="Username" onChange={handleUser} />
+      <input type="password" placeholder="Password" onChange={handlePass} />
       <div className="button-holder">
         <button onClick={handleLogin}>Login</button>
         <button onClick={handleSignUp}>Sign up</button>
