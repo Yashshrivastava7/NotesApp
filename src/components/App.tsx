c2063dbfc442e476f344f553c9abbdefdedde763
Yash Shrivastava <yash.shrivastava77@gmail.com>
2023-03-12 16:41:29 +0530

    Implemented login feature

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,16 +6,19 @@ import { NoteObject } from "../types/Types";
 import Login from "./Login";
 
 function App() {
-  const [notes, setNotes] = useState<NoteObject[]>([]);
+  // const [notes, setNotes] = useState<NoteObject[]>([]);
+  // return (
+  //   <div className="app-container">
+  //     <div className="todo-section">
+  //       <NewNoteSection notes={notes} setNotes={setNotes} />
+  //     </div>
+  //     <div className="todo-section">
+  //       <NoteList notes={notes} setNotes={setNotes}/>
+  //     </div>
+  //   </div>
+  // );
   return (
-    <div className="app-container">
-      <div className="todo-section">
-        <NewNoteSection notes={notes} setNotes={setNotes} />
-      </div>
-      <div className="todo-section">
-        <NoteList notes={notes} setNotes={setNotes}/>
-      </div>
-    </div>
+    <Login/>
   );
 }
 
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,16 +1,63 @@
 import { useState } from "react" ;
 import "../styles/Login.css" ;
 
-export default function Login() {
+type userPass = {
+  username : string ;
+  password : string ;
+}
+
+function Login() {
+  const [log , setLog] = useState<userPass[]>([]);
+  let id : string = "" ;
+  let pass : string = "" ;
+  const handleUser = (e : React.ChangeEvent<HTMLInputElement>) => {
+    id = e.target.value ;
+  }
+  const handlePass = (e : React.ChangeEvent<HTMLInputElement>) => {
+    pass = e.target.value ;
+  }
+  const handleLogin = () => {
+    let flag : number = 0;
+    log.map((n) => { 
+      if(n.username === id){
+        flag = 1;
+        if(n.password === pass) {
+          <p>
+            Login Successful
+          </p>
+        }
+        else {
+            <p className="red">
+              Wrong Password
+            </p>
+        }
+      }
+    });
+    if(log.length === 0 || flag === 0) {
+        <p className="red">
+          User does not exist
+        </p>
+    }
+    console.log(log);
+  }
+  const handleSignUp = () => {
+    setLog((old : userPass[]) : userPass[] => {
+      let newLog : userPass = { username : id , password : pass} ; 
+      return [newLog , ...old]
+    });
+    console.log(log);
+  }
   return (
     <div className="login-holder">
       <h1>Login or Sign Up</h1>
-      <input type="text" placeholder="Username"/>
-      <input type="password" placeholder="Password"/>
+      <input type="text" placeholder="Username" onChange={handleUser}/>
+      <input type="password" placeholder="Password" onChange={handlePass}/>
       <div className="button-holder">
-        <button>Login</button>
-        <button>Sign up</button>
+        <button onClick={handleLogin}>Login</button>
+        <button onClick={handleSignUp}>Sign up</button>
       </div>
     </div>
   );
 }
+
+export default Login;
